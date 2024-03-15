f5ad11b83b0edeb430b0957f979de7c237c7a66a
Brian <bjudaprawira@horizon.csueastbay.edu>
2024-03-14 22:03:31 -0700

    [3/14] First file upload for assessment

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
 import './App.css';
+import {isNameValid, getLocations} from './mock-api/apis.js';
 
-function App() {
+{/**instatiate empty location array */}
+let locations = []
+{/**fill in array calling getLocations api and assigning resolved promise data into locations array */}
+getLocations().then(location => {
+  locations = location
+})
+
+{/**boolean checker for name validation */}
+let nameCheck = true
+
+{/** function call to update name validation boolean */}
+function NameError(input) {
+  isNameValid(input).then(value => {
+    if(!value){
+      nameCheck = false
+    }
+  })
+}
+
+{/** render error message if boolean is false, otherwise leave empty */}
+function DisplayErrorMessage() {
+  let errorMessage = ''
+  if(!nameCheck) {
+    errorMessage = 'this name has already been taken'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nameErrorMessage">
+      <p>
+        {errorMessage}
+      </p>
     </div>
   );
 }
 
-export default App;
+{/** render name inputs and attach event handler to call valid name api */}
+function NameInput() {
+  return (
+    <div className="nameInput">
+      <label for="name">Name
+        <input id="name" onChange={e => NameError(e.target.value)}></input>
+      </label>
+      <DisplayErrorMessage />
+    </div>
+      
+  );
+}
+
+{/*render dropdown component */}
+function LocationDropdown() {
+  {/**map locations into fresh array to populate dropdown */}
+  let locationPlaces = locations.map(loc => <option value={loc} key={loc}>{loc}</option>)
+
+  return (
+    <div className="location">
+      <label for="location">Location
+        <select id="location">
+          {locationPlaces}
+        </select>
+      </label>
+    </div>
+  );
+}
+
+{/*render CTAs for form, not hooked up for form capture as not listed in requirements */}
+function FormCTAs() {
+  return (
+    <div className="formCTAs">
+      <button>Clear</button>
+      <button>Add</button>
+    </div>
+  );
+}
+
+{/*Basic table for display, no requirements to hook up and update table with user entered data */}
+function DataDisplay() {
+  return (
+    <div className="dataTable">
+      <table>
+        {/*hidden caption name for screen reader acessibility*/}
+        <caption>Name and Location Data</caption>
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Location</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
+  );
+}
+
+{/** react rendering function */}
+export default function App() {
+  return (
+    <div className="app">
+      <NameInput />
+      <LocationDropdown />
+      <FormCTAs />
+      <DataDisplay />
+    </div>
+  );
+}
